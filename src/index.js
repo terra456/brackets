fc8e8aa9cf335523e9547a012479cbb2864f14f3
terra456 <terra.456@yandex.ru>
2020-03-22 03:59:24 +0500

    feat: Implement function for checking brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const arr = Array.from(str);
+
+    for (let i=0; i<arr.length; i++) {
+
+        var twoArr = arr[i]+arr[i+1];
+
+        for (let j=0; j<bracketsConfig.length; j++) {
+            var con = bracketsConfig[j].join('');
+            if (twoArr == con) {
+                var removed = arr.splice(i, 2);
+                 i=i-2;
+
+            }
+
+        }
+
+            // } else if (arr[i] == ")" && arr[i-2] == "(") {
+            //     var removed = arr.splice(i-2, 3);
+            //     i = i-2;
+            // } else if (arr[i] == "}" && arr[i-2] == "{") {
+            //     var removed = arr.splice(i-2, 3);
+            //     i = i-2;
+            // } else if (arr[i] == "]" && arr[i-2] == "[") {
+            //     var removed = arr.splice(i-2, 3);
+            //     i = i-2;
+            // }
+
+
+     }
+
+
+    if (arr.length == 0) {
+        return true;
+
+    } else {
+        return false;
+    }
+
 }
